371c32be7247dff4bbfea05672e97f8150c0bdd1
halilakgul <halilakgul590@gmail.com>
2026-04-08 01:54:21 +0300

    Add all UI panels and building system

---
+++ core/src/main/java/com/nationarchitect/game/screens/ScreenTypes.java
@@ -0,0 +1,13 @@
+package com.nationarchitect.game.screens;
+
+public enum ScreenTypes {
+
+    MAIN_MENU, 
+    NEW_GAME, 
+    LOAD_GAME, 
+    MAIN_GAME, 
+    SETTINGS, 
+    CREDITS, 
+    PAUSE_MENU, 
+    GAME_OVER, 
+}
